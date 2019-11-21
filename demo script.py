1153962c23fcc2990bf9ba2fdb68e13f635d2387
edden-gerber <edden.gerber@gmail.com>
2019-11-21 19:07:37 +0200

    update readme, and cleanup

--- demo script.py
+++ demo script.py
@@ -11,7 +11,6 @@ import warnings
 
 from radical_shapley_values import compute_shapley_values
 from radical_shapley_values import shap_payoff_isolation_forest as shap_if
-from radical_shapley_values import shap_payoff_XGB as shap_xgb
 from radical_shapley_values import reshape_shapley_output
 
 
@@ -25,7 +24,8 @@ y = np.round(np.random.rand(num_samples))
 #%% Run exact-shapley function for isolation forest
 
 start = time.time()
-shapley_values = reshape_shapley_output(compute_shapley_values(shap_if, X))
+scores = shap_if(X)
+shapley_values = reshape_shapley_output(compute_shapley_values(shap_if, X, zero_payoff=np.ones(X.shape[0])*np.mean(scores)))
 end = time.time()
 print('Run time: %.3f' %(end-start))
 
@@ -53,38 +53,8 @@ feature_idx = 0
 plt.scatter(shapley_values[0:num_shap_samples,feature_idx], kernel_shap_values[0:num_shap_samples,feature_idx], s=5)
 mn = np.min(kernel_shap_values[:,feature_idx])
 mx = np.max(kernel_shap_values[:,feature_idx])
-plt.plot([mn,mx],[mn,mx],'r--')
+#plt.plot([mn,mx],[mn,mx],'r--')
 plt.xlabel('Radical Shapley values')
 plt.ylabel('Kernel Explainer SHAP values')
-
-
-#%% Run compute-shapley function for XGBoost
-start = time.time()
-shapley_values = reshape_shapley_output(compute_shapley_values(shap_xgb, X, y))
-end = time.time()
-print('Run time: %.3f' %(end-start))
-
-#%% Run SHAP library for XGBoost
-num_shap_samples = num_samples # number of samples for which to compute SHAP values. Total run time should be linear with this. 
-
-xgb_model = xgb.XGBRegressor(scale_pos_weight=1, \
-                          objective='binary:logistic', \
-                          eval_metric='auc', \
-                          max_depth=5, \
-                          n_estimators=100)
-
-xgb_model.fit(X, y)
-explainer = shap.TreeExplainer(xgb_model)
-start = time.time()
-tree_shap_values = explainer.shap_values(X.iloc[0:num_shap_samples])
-end = time.time()
-print('Run time: %.3f' %(end-start))
-
-#%% Plot radical Shapley vs SHAP values: 
-feature_idx = 0
-plt.scatter(shapley_values[0:num_shap_samples,feature_idx], tree_shap_values[0:num_shap_samples,feature_idx],s=5)
-mn = np.min(tree_shap_values[:,feature_idx])
-mx = np.max(tree_shap_values[:,feature_idx])
-plt.plot([mn,mx],[mn,mx],'r--')
-plt.xlabel('Radical Shapley values')
-plt.ylabel('Tree Explainer SHAP values')
+plt.xlim([-0.17, 0.09])
+plt.ylim([-0.17, 0.09])
